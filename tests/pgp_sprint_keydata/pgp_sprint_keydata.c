248d553aabfddf6551f8dbbb356bd61cbc43badb
Shaun Wheelhouse <shaun@sigio.is>
2017-03-28 11:38:00 +1100

    Updated dummy "test" to assume the netpgp context structure is clean.

--- tests/pgp_sprint_keydata/pgp_sprint_keydata.c
+++ tests/pgp_sprint_keydata/pgp_sprint_keydata.c
@@ -1,13 +1,11 @@
 
 #include <stddef.h>
-#include <string.h>
 #include <netpgp.h>
 
 int main(int argc, char *argv[])
 {
 	netpgp_t netpgp;
 
-	memset((void *) &netpgp, '\0', sizeof(netpgp));
 	if (netpgp_init(&netpgp)) {
 		netpgp_list_keys(&netpgp, 1);
 		netpgp_end(&netpgp);
